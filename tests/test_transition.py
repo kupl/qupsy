c38b13c364bc8eec04afed52c9eb7d09fde5a617
HaYeong Lee <110161434+glorialeezero@users.noreply.github.com>
2024-11-28 16:25:53 +0900

    Update test_transition.py

--- tests/test_transition.py
+++ tests/test_transition.py
@@ -5,6 +5,7 @@ from qupsy.language import (
     Add,
     Aexp,
     Cmd,
+    CRy,
     ForCmd,
     Gate,
     GateCmd,
@@ -13,7 +14,9 @@ from qupsy.language import (
     Integer,
     Pgm,
     SeqCmd,
+    Sub,
     Var,
+    X,
 )
 from qupsy.transition import next
 
@@ -150,4 +153,183 @@ def pgm(n):
     for i0 in range(1,n):
         CX(0, i0)
     """.strip()
+    )
+
+
+def test_inc_next():
+    pgm0 = Pgm("n")
+    pgm1 = Pgm("n", SeqCmd())
+    pgm2 = Pgm("n", SeqCmd(GateCmd()))
+    pgm3 = Pgm("n", SeqCmd(GateCmd(X())))
+    pgm4 = Pgm("n", SeqCmd(GateCmd(X(Integer(0)))))
+    pgm5 = Pgm("n", SeqCmd(GateCmd(X(Integer(0))), ForCmd("i0")))
+    pgm6 = Pgm("n", SeqCmd(GateCmd(X(Integer(0))), ForCmd("i0", Integer(0))))
+    pgm7 = Pgm("n", SeqCmd(GateCmd(X(Integer(0))), ForCmd("i0", Integer(0), Sub())))
+    pgm8 = Pgm(
+        "n",
+        SeqCmd(GateCmd(X(Integer(0))), ForCmd("i0", Integer(0), Sub(Var("n")))),
+    )
+    pgm9 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))), ForCmd("i0", Integer(0), Sub(Var("n"), Integer(1)))
+        ),
+    )
+    pgm10 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd("i0", Integer(0), Sub(Var("n"), Integer(1)), GateCmd()),
+        ),
+    )
+    pgm11 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd("i0", Integer(0), Sub(Var("n"), Integer(1)), GateCmd(CRy())),
+        ),
+    )
+    pgm12 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(CRy(Var("i0"))),
+            ),
+        ),
+    )
+    pgm13 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(CRy(Var("i0"), Add())),
+            ),
+        ),
+    )
+    pgm14 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(
+                    CRy(Var("i0"), Add(Var("i0"))),
+                ),
+            ),
+        ),
+    )
+    pgm15 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(
+                    CRy(Var("i0"), Add(Var("i0"), Integer(1))),
+                ),
+            ),
+        ),
+    )
+    pgm16 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(
+                    CRy(Var("i0"), Add(Var("i0"), Integer(1)), Integer(1)),
+                ),
+            ),
+        ),
+    )
+    pgm17 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(
+                    CRy(Var("i0"), Add(Var("i0"), Integer(1)), Integer(1), Sub()),
+                ),
+            ),
+        ),
+    )
+    pgm18 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(
+                    CRy(
+                        Var("i0"), Add(Var("i0"), Integer(1)), Integer(1), Sub(Var("n"))
+                    ),
+                ),
+            ),
+        ),
+    )
+    pgm19 = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(X(Integer(0))),
+            ForCmd(
+                "i0",
+                Integer(0),
+                Sub(Var("n"), Integer(1)),
+                GateCmd(
+                    CRy(
+                        Var("i0"),
+                        Add(Var("i0"), Integer(1)),
+                        Integer(1),
+                        Sub(Var("n"), Var("i0")),
+                    ),
+                ),
+            ),
+        ),
+    )
+
+    assert pgm1 in next(pgm0)
+    assert pgm2 in next(pgm1)
+    assert pgm3 in next(pgm2)
+    assert pgm4 in next(pgm3)
+    assert pgm5 in next(pgm4)
+    assert pgm6 in next(pgm5)
+    assert pgm7 in next(pgm6)
+    assert pgm8 in next(pgm7)
+    assert pgm9 in next(pgm8)
+    assert pgm10 in next(pgm9)
+    assert pgm11 in next(pgm10)
+    assert pgm12 in next(pgm11)
+    assert pgm13 in next(pgm12)
+    assert pgm14 in next(pgm13)
+    assert pgm15 in next(pgm14)
+    assert pgm16 in next(pgm15)
+    assert pgm17 in next(pgm16)
+    assert pgm18 in next(pgm17)
+    assert pgm19 in next(pgm18)
+    assert (
+        str(pgm19)
+        == """
+def pgm(n):
+    X(0)
+    for i0 in range(0,(n - 1)):
+        CRy(i0, (i0 + 1), 1, (n - i0))
+        """.strip()
     )
