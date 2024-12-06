3d1570e2da83d1f08cdd79915a71a170733b91ea
HaYeong Lee <110161434+glorialeezero@users.noreply.github.com>
2024-12-06 13:45:23 +0900

    Update test_search.py

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,9 +1,25 @@
 from qupsy.language import CX, ForCmd, GateCmd, H, Integer, Pgm, SeqCmd, Var
 from qupsy.search import search
-from qupsy.spec import make_spec, SpecData
+from qupsy.spec import SpecData, make_spec
+
+raw_spec: SpecData = {
+    "gates": ["H", "CX"],
+    "testcases": {
+        "1": {
+            "input": None,
+            "output": "0.70710677,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.70710677",
+        },
+        "2": {
+            "input": None,
+            "output": "0.70710677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.70710677",
+        },
+        "3": {"input": None, "output": "0.70710677,0,0,0,0,0,0,0.70710677"},
+    },
+}
+spec = make_spec(raw_spec)
 
 
-def test_search_ghz_last_step():
+def test_search_ghz_one_step():
     init_pgm = Pgm(
         "n",
         SeqCmd(
@@ -18,26 +34,12 @@ def test_search_ghz_last_step():
             ForCmd("i0", Integer(1), Var("n"), GateCmd(CX(Integer(0), Var("i0")))),
         ),
     )
-    raw_spec: SpecData = {
-        "gates": ["H", "CX"],
-        "testcases": {
-            "1": {
-                "input": None,
-                "output": "0.70710677,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.70710677",
-            },
-            "2": {
-                "input": None,
-                "output": "0.70710677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.70710677",
-            },
-            "3": {"input": None, "output": "0.70710677,0,0,0,0,0,0,0.70710677"},
-        },
-    }
-    spec = make_spec(raw_spec)
+
     synthesized_pgm = search(spec, initial_pgm=init_pgm)
     assert synthesized_pgm == final_pgm
 
 
-def test_search_ghz_second_last_step():
+def test_search_ghz_two_step():
     init_pgm = Pgm(
         "n",
         SeqCmd(
@@ -52,20 +54,23 @@ def test_search_ghz_second_last_step():
             ForCmd("i0", Integer(1), Var("n"), GateCmd(CX(Integer(0), Var("i0")))),
         ),
     )
-    raw_spec: SpecData = {
-        "gates": ["H", "CX"],
-        "testcases": {
-            "1": {
-                "input": None,
-                "output": "0.70710677,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.70710677",
-            },
-            "2": {
-                "input": None,
-                "output": "0.70710677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.70710677",
-            },
-            "3": {"input": None, "output": "0.70710677,0,0,0,0,0,0,0.70710677"},
-        },
-    }
-    spec = make_spec(raw_spec)
+
+    synthesized_pgm = search(spec, initial_pgm=init_pgm)
+    assert synthesized_pgm == final_pgm
+
+
+def test_search_ghz_three_step():
+    init_pgm = Pgm(
+        "n",
+        SeqCmd(GateCmd(H(Integer(0))), ForCmd("i0", Integer(1), Var("n"), GateCmd())),
+    )
+    final_pgm = Pgm(
+        "n",
+        SeqCmd(
+            GateCmd(H(Integer(0))),
+            ForCmd("i0", Integer(1), Var("n"), GateCmd(CX(Integer(0), Var("i0")))),
+        ),
+    )
+
     synthesized_pgm = search(spec, initial_pgm=init_pgm)
     assert synthesized_pgm == final_pgm
